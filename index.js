dc04002b8768b97cea7d5eb861cbc7ce9e18f9e8
himanshuhere <himanshusoni3105@outlook.com>
2020-07-27 15:37:26 +0530

    after morgan

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express'),
-    http = require('http');
+    http = require('http'),
+    morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3100;
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>Hi this is express running..</h1></body></html>');
